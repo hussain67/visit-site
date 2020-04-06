6ab927984df7f916015d67e97393d176d274d43b
Shahid <hussain.msh67@yahoo.com>
2020-04-06 20:15:47 +0100

    Reveal on scroll completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,17 @@
 import '../styles/styles.scss'
 import Person from './modules/Person'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+let mobileMenu = new MobileMenu();
+
 
 if (module.hot){
     module.hot.accept()
 }
 
-let mobileMenu = new MobileMenu();
 
 
 
