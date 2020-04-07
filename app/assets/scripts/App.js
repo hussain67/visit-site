48b357e56d0dc86dbea5360c1d50708942f978cc
Shahid <hussain.msh67@yahoo.com>
2020-04-07 19:33:21 +0100

    Modal Completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,11 +6,29 @@ import StickyHeader from './modules/StickyHeader'
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+ new MobileMenu();
+ new StickyHeader();
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+
+        if(typeof modal == "undefined"){
+            import('./modules/Modal').then( x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)  
+       
+               } ).catch(()=> console.log('There was a problem')) 
+        }else{
+            modal.openTheModal()
+        }
+        
+    })
+})
 
 
-if (module.hot){
+if (module.hot) {
     module.hot.accept()
 }
 
